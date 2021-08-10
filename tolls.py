658d8d1b51f74ea5739e8a240062ee02a9b19c9c
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2021-08-11 02:27:31 +0300

    Update tolls.py

--- tolls.py
+++ tolls.py
@@ -1,5 +1,41 @@
 
 import os
+def gimatrya(word:str)->set:
+  gimHash = dict()
+  gimHash["א"]="1"
+  gimHash["ב"]="2"
+  gimHash["ג"]="3"
+  gimHash["ד"]="4"
+  gimHash["ה"]="5"
+  gimHash["ו"]="6"
+  gimHash["ז"]="7"
+  gimHash["ח"]="8"
+  gimHash["ט"]="9"
+  gimHash["י"]="10"
+  gimHash["כ"]="20"
+  gimHash["ל"]="30"
+  gimHash["מ"]="40"
+  gimHash["נ"]="50"
+  gimHash["ס"]="60"
+  gimHash["ע"]="70"
+  gimHash["פ"]="80"
+  gimHash["צ"]="90"
+  gimHash["ק"]="100"
+  gimHash["ר"]="200"
+  gimHash["ש"]="300"
+  gimHash["ת"]="400"
+  gimHash["ך"]="500"
+  gimHash["ם"]="600"
+  gimHash["ן"]="700"
+  gimHash["ף"]="800"
+  gimHash["ץ"]="900"
+  chars = list(word)
+  return {gimHash[x] for x in chars}
+    
+def classesbyWord(word:str, classes:dict):
+  gimclasses = gimatrya(word)
+  return set([str(classIndex[x]) for x in gimclasses])
+
 def detectWord(word):
   # word = list(word)
   # word.reverse()
