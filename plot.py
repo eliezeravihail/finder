a2eeddc18d6596c4709c0153737d89eac2c81502
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2021-08-11 18:20:56 +0300

    Update plot.py
    remove label and set color to RGB(0,52,113) is nice blue

--- plot.py
+++ plot.py
@@ -42,21 +42,6 @@ class Colors:
 
 colors = Colors()  # create instance for 'from utils.plots import colors'
 
-# my edited on plot one box
-def plot_one_word(x, im, color=(128, 128, 128), label=None, line_thickness=3):
-    # Plots one bounding box on image 'im' using OpenCV
-    assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
-    tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  # font thickness
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-
-
 
 def hist2d(x, y, n=100):
     # 2d histogram used in labels.png and evolve.png
@@ -67,6 +52,8 @@ def hist2d(x, y, n=100):
     return np.log(hist[xidx, yidx])
 
 def plot_one_word(x,y,im, color=(128, 128, 128), label=None, line_thickness=3):
+    color=(113, 52, 0)
+    label=None
     # Plots one bounding box on image 'im' using OpenCV
     assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
     tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
