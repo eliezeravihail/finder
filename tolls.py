4c3b8c1ed7520f8e50bb86216feea4f5c5e8545f
eliezer avihail <48094022+eliezeravihail@users.noreply.github.com>
2021-08-11 14:47:15 +0300

    Update tolls.py

--- tolls.py
+++ tolls.py
@@ -1,3 +1,5 @@
+
+
 #myadded
 dictLabel = dict()
 """Run inference with a YOLOv5 model on images, videos, directories, streams
@@ -22,7 +24,7 @@ from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, colorstr, non_max_suppression, \
     apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, save_one_box
-from utils.plots import colors, plot_one_box
+from utils.plots import colors, plot_one_box,  plot_one_word
 from utils.torch_utils import select_device, load_classifier, time_sync
 gimHashl = dict()
 gimHashl["a"]="1"
@@ -292,8 +294,7 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
                     else:
                       flag = False
                   if flag:
-                    for j in i:
-                      plot_one_box(j[1], im0, label=intToGimL(j[0].split()[0]), color=colors(4, True), line_thickness=line_thickness)
+                      plot_one_word(i[0], im0, label=intToGimL(j[0].split()[0]), color=colors(4, True), line_thickness=line_thickness)
                     
                
             # print("\nthe dict is:\n",dictLabel)
